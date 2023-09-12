7ca613c49ff417428e78fe03134d41baf215df3e
Java-Instructor <143517387+Java-instructor@users.noreply.github.com>
2023-09-12 12:03:25 +0530

    Update SpringRedisCacheApplication.java

--- src/main/java/com/java/instructor/spring/redis/SpringRedisCacheApplication.java
+++ src/main/java/com/java/instructor/spring/redis/SpringRedisCacheApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
 
 @SpringBootApplication
-@EnableCaching
+@EnableCaching  // if this annotation not enabled. then cache will not work even if you used any other cache related notation. 
 public class SpringRedisCacheApplication {
 
 	public static void main(String[] args) {
